91f256ab01dc5ab65feddbcae8549f4351e5d02c
Colton <coltonkaraffa@gmail.com>
2023-05-23 15:49:17 -0400

    .

---
+++ Question881.java
@@ -0,0 +1,16 @@
+public class Question881 {
+    public static void main(String[] args) {
+        Solution881 s = new Solution881();
+        int[] people = {1,2};
+        int limit = 3;
+        int result = s.numRescueBoats(people, limit);
+        System.out.println(result);
+    }
+}
+
+class Solution881 {
+    public int numRescueBoats(int[] people, int limit) {
+
+        return 0;
+    }
+}
