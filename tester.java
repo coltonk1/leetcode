2010dbf83001f456effcf4dd9f6111101966ff77
Colton <coltonkaraffa@gmail.com>
2023-05-23 17:33:02 -0400

    update

---
+++ tester.java
@@ -0,0 +1,8 @@
+public class tester {
+    public static void main(String[] args) {
+        Solution881 s = new Solution881();
+        int[] a = {2,3,5,1,6};
+        int b = 5;
+        System.out.println(s.numRescueBoats(a,b));
+    }
+}
