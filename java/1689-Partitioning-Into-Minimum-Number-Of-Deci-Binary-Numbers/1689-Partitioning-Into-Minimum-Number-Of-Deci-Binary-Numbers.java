ec7956267f12e37adce014e1a4f4f0b17cd170a7
Colton <81594541+coltonk1@users.noreply.github.com>
2023-05-25 16:37:56 -0400

    Time: 7 ms (78.40%), Space: 44.2 MB (8.17%)

--- java/1689-Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers/1689-Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers.java
+++ java/1689-Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers/1689-Partitioning-Into-Minimum-Number-Of-Deci-Binary-Numbers.java
@@ -1,9 +1,14 @@
 class Solution {
     public int minPartitions(String n) {
         int max = 0;
-        for(char c : n.toCharArray()){
-            max = Math.max(c-48, max);
+        for(int i = 9; i >= 0; i--){
+            if(n.indexOf(i + "") > -1){
+                return i;
+            }
         }
+        // for(char c : n.toCharArray()){
+        //     max = Math.max(c-48, max);
+        // }
         return max;
     }
 }
