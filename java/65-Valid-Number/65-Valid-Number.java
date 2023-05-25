5a8d010d4ec0b941aa6630823d5a96c9ec48498c
Colton <81594541+coltonk1@users.noreply.github.com>
2023-05-25 04:43:58 -0400

    Time: 2 ms (96.76%), Space: 42.5 MB (71.31%)

--- java/65-Valid-Number/65-Valid-Number.java
+++ java/65-Valid-Number/65-Valid-Number.java
@@ -3,20 +3,17 @@ class Solution {
         if(s.length() > 1 && (s.charAt(0) == '-' || s.charAt(0) == '+')){
             if((s.charAt(1) < '0' || s.charAt(1) > '9') && s.charAt(1) != '.')
                 return false;
-            
-            if(s.charAt(1) == '.'){
-                if(s.length() <= 2)
-                    return false;
-                if(s.charAt(2) < '0' || s.charAt(2) > '9')
-                    return false;
-            }
+            if(s.charAt(1) == '.' && (s.length() <= 2 || (s.charAt(2) < '0' || s.charAt(2) > '9')))
+                return false;
         }
+        
         if(s.length() == 1){
             return !(s.charAt(0) < '0' || s.charAt(0) > '9');
         }
 
         if(s.charAt(0) == '.' && (s.charAt(1) == 'e' || s.charAt(1) == 'E'))
             return false;
+
         int dotcount = 0;
         int ecount = 0;
         for(int i = 0; i < s.length(); i++){
@@ -28,16 +25,15 @@ class Solution {
                     continue;
                 if(s.charAt(i-1) == 'e' || s.charAt(i-1) == 'E')
                     continue;
-                else
-                    return false;
+                return false;
             }
             else if(i > 0 && (c == 'e' || c == 'E') && ecount == 0){
                 ecount++;
                 dotcount++;
             if(i == s.length()-1 || ((s.charAt(i-1) < '0' || s.charAt(i-1) > '9') && s.charAt(i-1) != '.'))
                 return false;
-            if(!(s.charAt(i+1) >= '0' && s.charAt(i+1) <= '9') && !(s.charAt(i+1) == '-' || s.charAt(i+1) == '+'))
-                return false;
+            if((s.charAt(i+1) >= '0' && s.charAt(i+1) <= '9'))
+                continue;
 
             if((s.charAt(i+1) == '-' || s.charAt(i+1) == '+')){
                 if(i == s.length()-2)
