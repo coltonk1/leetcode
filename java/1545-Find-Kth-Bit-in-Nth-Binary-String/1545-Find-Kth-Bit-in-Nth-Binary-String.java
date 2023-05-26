1509d5ae83512c104f8cc0b2c19a358122f13fb0
Colton <81594541+coltonk1@users.noreply.github.com>
2023-05-26 03:08:19 -0400

    Time: 174 ms (21.9%), Space: 53.6 MB (5.9%)

---
+++ java/1545-Find-Kth-Bit-in-Nth-Binary-String/1545-Find-Kth-Bit-in-Nth-Binary-String.java
@@ -0,0 +1,22 @@
+class Solution {
+    public char findKthBit(int n, int k) {
+        StringBuilder sn = new StringBuilder("0");
+        for(int i = 0; i < n-1; i++){
+            sn = new StringBuilder(sn + "1" + invert(sn));
+        }
+        return sn.charAt(k-1);
+    }
+
+    public StringBuilder invert(StringBuilder sn){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < sn.length(); i++){
+            char c = sn.charAt(i);
+            if(c == '1')
+                c = '0';
+            else
+                c = '1';
+            sb.append(c);
+        }
+        return sb.reverse();
+    }
+}
