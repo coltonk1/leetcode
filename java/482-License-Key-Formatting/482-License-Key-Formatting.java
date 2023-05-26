d1a667dad08624e4d9225728bdfbcb56d490edc4
Colton <81594541+coltonk1@users.noreply.github.com>
2023-05-25 23:39:14 -0400

    Time: 241 ms (17.76%), Space: 45.2 MB (5.11%)

--- java/482-License-Key-Formatting/482-License-Key-Formatting.java
+++ java/482-License-Key-Formatting/482-License-Key-Formatting.java
@@ -2,10 +2,18 @@ class Solution {
     public String licenseKeyFormatting(String s, int k) {
         s = s.toUpperCase().replaceAll("-", "");
         int end = (s.length()-1)/k;
-        int initLength = s.length();
-        for(int i = 1; i <= end; i++){
-            s = s.substring(0, initLength-i*k) + "-" + s.substring(initLength-i*k, initLength+i-1);
+        // int initLength = s.length();
+        // for(int i = 1; i <= end; i++){
+        //     s = s.substring(0, initLength-i*k) + "-" + s.substring(initLength-i*k, initLength+i-1);
+        // }
+        String result = "";
+        for(int i = s.length()-1; i >= 0; i--){
+            if(((s.length()-i-1)%k) == 0 && i != s.length()-1){
+                result = s.charAt(i) + "-" + result;
+            }
+            else
+                result = s.charAt(i) + result;
         }
-        return s;
+        return result;
     }
 }
