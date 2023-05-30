f8290e276010c3b5837d018f1e0b90f7cdf7db87
Colton <81594541+coltonk1@users.noreply.github.com>
2023-05-29 23:37:33 -0400

    Time: 2 ms (20.83%), Space: 40.1 MB (49.64%)

--- java/7-Reverse-Integer/7-Reverse-Integer.java
+++ java/7-Reverse-Integer/7-Reverse-Integer.java
@@ -1,20 +1,14 @@
 class Solution {
     public int reverse(int x) {
         boolean negative = false;
-        if(x < 0)
-            negative = true;
+        if(x < 0) negative = true;
+        if(x == Integer.MIN_VALUE) return 0;
 
-        if(x == Integer.MIN_VALUE)
-            return 0;
-        x = Math.abs(x);
-        StringBuilder num = new StringBuilder(Integer.toString(x));
-        num = num.reverse();
-        long result = Long.parseLong(num.toString());
+        StringBuilder num = new StringBuilder(Integer.toString(Math.abs(x)));
+        long result = Long.parseLong(num.reverse().toString());
 
-        if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
-            return 0;
+        if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) return 0;
 
         return (int)result * (negative ? -1 : 1);
-
     }
 }
