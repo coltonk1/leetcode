c6fda18e02ca24b27a679378f1b5bab15be40133
Colton <81594541+coltonk1@users.noreply.github.com>
2023-05-29 22:20:35 -0400

    Time: 3 ms (96.9%), Space: 41.9 MB (99.68%)

--- java/3-Longest-Substring-Without-Repeating-Characters/3-Longest-Substring-Without-Repeating-Characters.java
+++ java/3-Longest-Substring-Without-Repeating-Characters/3-Longest-Substring-Without-Repeating-Characters.java
@@ -1,24 +1,12 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        char[] sArr = s.toCharArray();
-        if(sArr.length == 0)
-            return 0;
+        if(s.length() == 0) return 0;
         int max = 0;
-        int left = 0;
-        boolean cont = true;
-        while(cont){
-            for(int i = left; i < sArr.length; i++){
-                int index = s.indexOf(sArr[i], left);
-                if(index < i){
-                    max = Math.max(max, i-left);
-                    left = index+1;
-                    break;
-                }
-                if(i == sArr.length-1){
-                    cont = false;
-                    max = Math.max(max, i+1-left);
-                }
-            }
+        for(int left = 0, right = 0; right < s.length(); right++){
+            int index = s.indexOf(s.charAt(right), left);
+            if(index < right)
+                left = index+1;
+            max = Math.max(max, right-left+1);
         }
         return max;
     }
