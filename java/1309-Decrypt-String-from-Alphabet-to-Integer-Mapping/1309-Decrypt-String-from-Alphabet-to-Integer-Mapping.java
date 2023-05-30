05945b9d25eac22297a7cd58e1288a461ad2746e
Colton <81594541+coltonk1@users.noreply.github.com>
2023-05-30 03:05:09 -0400

    Time: 2 ms (55.82%), Space: 41 MB (62.58%)

--- java/1309-Decrypt-String-from-Alphabet-to-Integer-Mapping/1309-Decrypt-String-from-Alphabet-to-Integer-Mapping.java
+++ java/1309-Decrypt-String-from-Alphabet-to-Integer-Mapping/1309-Decrypt-String-from-Alphabet-to-Integer-Mapping.java
@@ -1,17 +1,16 @@
 class Solution {
     public String freqAlphabets(String s) {
-        String result = "";
+        StringBuilder result = new StringBuilder("");
         for(int i = s.length()-1; i >= 0; i--){
+            char c = ' ';
             if(s.charAt(i) == '#'){
-                char c = (char)(Integer.parseInt(s.substring(i-2, i))+'a'-1);
-                result = c + result;
+                c = (char)(Integer.parseInt(s.substring(i-2, i))+'a'-1);
                 i-=2;
             }
-            else{
-                char c = (char)(Integer.parseInt(s.charAt(i)+"")+'a'-1);
-                result = c + result;
-            }
+            else
+                c = (char)(Integer.parseInt(s.charAt(i)+"")+'a'-1);
+            result.insert(0, c);
         }
-        return result;
+        return result.toString();
     }
 }
