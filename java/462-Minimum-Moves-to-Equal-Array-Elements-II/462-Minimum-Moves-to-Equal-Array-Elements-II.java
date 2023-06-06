0ef12ee1baae7c5be02874949364b022e9fb2ae5
Colton <81594541+coltonk1@users.noreply.github.com>
2023-06-06 14:35:15 -0400

    Time: 5 ms (44.98%), Space: 43.5 MB (50.93%)

---
+++ java/462-Minimum-Moves-to-Equal-Array-Elements-II/462-Minimum-Moves-to-Equal-Array-Elements-II.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int median = nums[nums.length/2];
+        int result = 0;
+        for(int i : nums){
+            result += Math.abs(i-median);
+        }
+        return result;
+    }
+}
