7053bdb74aacebd88d880fc45a7ac3aae93539d8
Colton <81594541+coltonk1@users.noreply.github.com>
2023-06-06 14:36:29 -0400

    Time: 4 ms (84.20%), Space: 44 MB (16.36%)

--- java/462-Minimum-Moves-to-Equal-Array-Elements-II/462-Minimum-Moves-to-Equal-Array-Elements-II.java
+++ java/462-Minimum-Moves-to-Equal-Array-Elements-II/462-Minimum-Moves-to-Equal-Array-Elements-II.java
@@ -3,9 +3,7 @@ class Solution {
         Arrays.sort(nums);
         int median = nums[nums.length/2];
         int result = 0;
-        for(int i : nums){
-            result += Math.abs(i-median);
-        }
+        for(int i : nums) result += Math.abs(i-median);
         return result;
     }
 }
