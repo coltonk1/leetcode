af56122b3513e558a0ef705dec824472a4690ab7
Colton <81594541+coltonk1@users.noreply.github.com>
2023-06-19 18:25:51 -0400

    Time: 4 ms (61.80%), Space: 42.5 MB (42.66%)

--- java/38-Count-and-Say/38-Count-and-Say.java
+++ java/38-Count-and-Say/38-Count-and-Say.java
@@ -1,14 +1,14 @@
 class Solution {
     public String countAndSay(int n) {
         if(n == 1) return "1";
-        String current = "1";
+        StringBuilder current = new StringBuilder("1");
         for(int i = 0; i < n-1; i++){
             current = say(current);
         }
-        return current;
+        return current.toString();
     }
 
-    public String say(String s){
+    public StringBuilder say(StringBuilder s){
         StringBuilder res = new StringBuilder("");
         int amt = 1;
         for(int i = 1; i < s.length(); i++){
@@ -21,6 +21,6 @@ class Solution {
         }
         res.append(Integer.toString(amt));
         res.append(s.charAt(s.length()-1));
-        return res.toString();
+        return res;
     }
 }
