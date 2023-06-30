f1997424ce55492edeaf1917629da0c2929fd3c8
Colton <81594541+coltonk1@users.noreply.github.com>
2023-06-30 18:25:46 -0400

    Time: 3 ms (98.74%), Space: 57.6 MB (40.34%)

--- java/1306-Jump-Game-III/1306-Jump-Game-III.java
+++ java/1306-Jump-Game-III/1306-Jump-Game-III.java
@@ -1,18 +1,8 @@
 class Solution {
-    boolean valid = false;
-    public boolean canReach(int[] arr, int start) {
-        Map<Integer, Integer> hash = new HashMap<Integer, Integer>();
-        l(arr, start, hash);
-        return valid;
-    }
-    public void l(int[] arr, int current, Map<Integer, Integer> hash){
-        if(valid)
-            return;
-        if(hash.containsKey(current)) return;
-        hash.put(current, 1);
-        if(current >= arr.length || current < 0) return;
-        if(arr[current] == 0) valid = true;
-        l(arr, current+arr[current], hash);
-        l(arr, current-arr[current], hash);
+    public boolean canReach(int[] arr, int current) {
+        if(current >= arr.length || current < 0 || arr[current] < 0) return false;
+        arr[current] = -arr[current];
+        if(arr[current] == 0) return true;
+        return canReach(arr, current+arr[current]) || canReach(arr, current-arr[current]);
     }
 }
