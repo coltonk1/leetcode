963f10a5b7f8cdc48965c83137f631578a3da735
Colton <81594541+coltonk1@users.noreply.github.com>
2023-07-07 00:57:16 -0400

    Time: 131 ms (6.94%), Space: 40.9 MB (58.9%)

--- java/1507-Reformat-Date/1507-Reformat-Date.java
+++ java/1507-Reformat-Date/1507-Reformat-Date.java
@@ -1,11 +1,21 @@
 class Solution {
     public String reformatDate(String date) {
         String[] args = date.split(" ");
-        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-        String month = (Arrays.asList(months).indexOf(args[1])+1)+"";
+        Map<String, String> months = new HashMap<String, String>();
+        months.put("Jan", "01");
+        months.put("Feb", "02");
+        months.put("Mar", "03");
+        months.put("Apr", "04");
+        months.put("May", "05");
+        months.put("Jun", "06");
+        months.put("Jul", "07");
+        months.put("Aug", "08");
+        months.put("Sep", "09");
+        months.put("Oct", "10");
+        months.put("Nov", "11");
+        months.put("Dec", "12");
         String day = args[0].substring(0, args[0].length()-2);
         if(day.length() == 1) day = "0" + day;
-        if(month.length() == 1) month = "0" + month;
-        return args[2]+"-"+month+"-"+day;
+        return args[2]+"-"+months.get(args[1])+"-"+day;
     }
 }
