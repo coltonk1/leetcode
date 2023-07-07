cbdf04c9b03c818b7285108e132fa5d86b4952e2
Colton <81594541+coltonk1@users.noreply.github.com>
2023-07-07 13:23:29 -0400

    Time: 2 ms (50.29%), Space: 40.4 MB (84.48%)

--- java/1700-Number-of-Students-Unable-to-Eat-Lunch/1700-Number-of-Students-Unable-to-Eat-Lunch.java
+++ java/1700-Number-of-Students-Unable-to-Eat-Lunch/1700-Number-of-Students-Unable-to-Eat-Lunch.java
@@ -1,12 +1,15 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
+        Arrays.sort(students);
         List<Integer> sand = new ArrayList<>();
         List<Integer> stud = new ArrayList<>();
         for(int i : sandwiches) sand.add(i);
         for(int i : students) stud.add(i);
         int to = sandwiches.length;
         for(int i = 0; i < to; i++){
-            int index = stud.indexOf(sand.get(0));
+            int index = -1;
+            if(stud.get(0) == sand.get(0)) index=0;
+            else if(stud.get(stud.size()-1) == sand.get(0)) index=stud.size()-1;
             if(index < 0) break;
             stud.remove(index);
             sand.remove(0);
