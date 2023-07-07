605a940863b62c399d68096fc6b54233f43549be
Colton <81594541+coltonk1@users.noreply.github.com>
2023-07-07 00:59:55 -0400

    Time: 7 ms (34.10%), Space: 40.8 MB (82.37%)

--- java/1507-Reformat-Date/1507-Reformat-Date.java
+++ java/1507-Reformat-Date/1507-Reformat-Date.java
@@ -14,8 +14,14 @@ class Solution {
         months.put("Oct", "10");
         months.put("Nov", "11");
         months.put("Dec", "12");
+        StringBuilder result = new StringBuilder("");
+        result.append(args[2]);
+        result.append("-");
+        result.append(months.get(args[1]));
+        result.append("-");
         String day = args[0].substring(0, args[0].length()-2);
         if(day.length() == 1) day = "0" + day;
+        result.append(day);
         return args[2]+"-"+months.get(args[1])+"-"+day;
     }
 }
