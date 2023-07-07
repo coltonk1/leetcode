1b6afbccd4b575c284e5aaa86ce124f3fec26409
Colton <81594541+coltonk1@users.noreply.github.com>
2023-07-07 00:43:20 -0400

    Time: 2 ms (100%), Space: 44.4 MB (71.40%)

--- java/845-Longest-Mountain-in-Array/845-Longest-Mountain-in-Array.java
+++ java/845-Longest-Mountain-in-Array/845-Longest-Mountain-in-Array.java
@@ -1,35 +1,24 @@
 class Solution {
     public int longestMountain(int[] arr) {
-        int max = 0;
         if(arr.length == 1) return 0;
+        int max = 0;
         boolean increasing = false;
         boolean valid = false;
-        int start = 0;
+        int startIndex = 0;
         for(int i = 1; i < arr.length; i++){
             if(arr[i]>arr[i-1]){
-                if(!increasing){
-                    if(valid) max = Math.max(max, i-start);
-                    start = i-1;
-                }
-                if(!valid)
-                    start = i-1;
+                if(!increasing && valid) max = Math.max(max, i-startIndex);
+                if(!valid || !increasing) startIndex = i-1;
                 increasing = true;
                 valid = true;
             }
             else if(arr[i]==arr[i-1]){
-                if(!increasing && valid){
-                    max = Math.max(max, i-start);
-                }
+                if(!increasing && valid) max = Math.max(max, i-startIndex);
                 valid = false;
             }
-            else{
-                increasing = false;
-            }
+            else increasing = false;
         }
-        if(!increasing && valid){
-            max = Math.max(max, arr.length-start);
-        }
-        if(max == 1) return 0;
-        return max;
+        if(!increasing && valid) max = Math.max(max, arr.length-startIndex);
+        return max<=1?0:max;
     }
 }
