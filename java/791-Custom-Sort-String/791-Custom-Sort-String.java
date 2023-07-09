7957c3a52ff7dced4e9170210b1ca85c8b7c52d7
Colton <81594541+coltonk1@users.noreply.github.com>
2023-07-08 23:25:14 -0400

    Time: 4 ms (38.48%), Space: 41 MB (43.47%)

--- java/791-Custom-Sort-String/791-Custom-Sort-String.java
+++ java/791-Custom-Sort-String/791-Custom-Sort-String.java
@@ -1,13 +1,17 @@
 class Solution {
     public String customSortString(String order, String s) {
         StringBuilder output = new StringBuilder("");
+        Map<Character, Integer> letters = new HashMap<Character, Integer>();
+        for(char c : s.toCharArray()){
+            letters.put(c, letters.getOrDefault(c, 0) + 1);
+        }
         for(int i = 0; i < order.length(); i++){
-            while(s.indexOf(order.charAt(i)) >= 0){
-                output.append(order.charAt(i));
-                s=s.replaceFirst(order.charAt(i)+"", "");
+            char c = order.charAt(i);
+            for(int j = 0; j < letters.getOrDefault(c, 0); j++){
+                output.append(c);
             }
         }
-        output.append(s);
+        output.append(s.replaceAll("["+order+"]", ""));
         return output.toString();
     }
 }
