8c0e2518a933c61551e3b7e6b1efc19675e9c337
Colton <81594541+coltonk1@users.noreply.github.com>
2023-07-27 15:31:16 -0400

    Time: - ms (Beats 100.00%), Space: 38.97 ms (95.17%)

--- java/2481-Minimum-Cuts-to-Divide-a-Circle/2481-Minimum-Cuts-to-Divide-a-Circle.java
+++ java/2481-Minimum-Cuts-to-Divide-a-Circle/2481-Minimum-Cuts-to-Divide-a-Circle.java
@@ -1,1 +1,7 @@
-class Solution {    public int numberOfCuts(int n) {        if(n == 1) return 0;        if(n%2 == 1) return n;        return n/2;    }}
+class Solution {
+    public int numberOfCuts(int n) {
+        if(n == 1) return 0;
+        if(n%2 == 1) return n;
+        return n/2;
+    }
+}
