7598508dced50f3ebb40477f420d3ff6c12bd5fe
Colton <81594541+coltonk1@users.noreply.github.com>
2023-07-27 15:48:12 -0400

    Time: 1 ms (100.00%), Space: 44.96 ms (8.05%)

---
+++ java/724-Find-Pivot-Index/724-Find-Pivot-Index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int frontTotal = 0;
+        for(int i = 0; i < nums.length; i++){
+            frontTotal += nums[i];
+        }
+        int backTotal = 0;
+        for(int i = 0; i < nums.length; i++){
+            frontTotal -= nums[i];
+            if(frontTotal == backTotal) return i;
+            backTotal += nums[i];
+        }
+        return -1;
+    }
+}
